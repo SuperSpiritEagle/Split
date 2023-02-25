ff7068ad6758341a2116e525b7516e47005f34a1
SuperSpiritEagle <118557578+SuperSpiritEagle@users.noreply.github.com>
2023-02-25 11:49:51 +0500

    init

---
+++ Split/Split/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Split
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Мороз и солнце день чудесный";
+
+            string[] words = text.Split(new char[] { });
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
